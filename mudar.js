5b46e3364531192ebb540de80d15d758645dc03a
Yasmin Lopes Borba <yasmin.b0rbalopes@gmail.com>
2024-10-08 08:27:21 -0300

    clone starbucks

--- mudar.js
+++ mudar.js
@@ -1,12 +1,13 @@
-//primeira parte Habilitado/Desabilitado
+// primeira parte Habilitado/Desabilitado
 function toggleMenu(){
     var menuToggle = document.querySelector('.toggle');
-    var links = document.querySelector('.links')
+    var links = document.querySelector('.links');
     // troca o estado da classe .toggle para ativo
     menuToggle.classList.toggle('active')
-    // troca o estado da classe .links para ativo
+    // troca o estado da classe .links para ativo 
     links.classList.toggle('active')
 }
+
 // troca o copo grande 
 function imgSlider(anything){
     document.querySelector('.starbucks').src = anything;
